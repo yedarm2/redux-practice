5a98586ccb477b97d40fb197902717bbf4ba4347
yedarm <oncoder@naver.com>
2022-02-17 22:27:27 +0900

    로직 수정

--- components/TodoList/TodoList.tsx
+++ components/TodoList/TodoList.tsx
@@ -2,36 +2,49 @@ import { FC } from 'react';
 
 import { Box, List, ListItem, ListItemButton } from '@mui/material';
 
-import { IRawTodo } from '../../types';
-
-const todoList: IRawTodo[] = [
-	{
-		work: '할 일 1',
-	},
-	{
-		work: '할 일 2',
-	},
-	{
-		work: '할 일 3',
-	},
-	{
-		work: '할 일 4',
-	},
-	{
-		work: '할 일 5',
-	},
-];
+import { ITodo } from '../../types';
 
 export const TodoList: FC = () => {
+	const { todoList } = useTodoList();
+
 	return (
 		<Box>
 			<List>
-				{todoList.map((todo, index) => (
-					<ListItem key={index}>
+				{todoList.map(todo => (
+					<ListItem key={todo.id}>
 						<ListItemButton>{todo.work}</ListItemButton>
 					</ListItem>
 				))}
 			</List>
 		</Box>
 	);
+};
+
+const useTodoList = () => {
+	const todoList: ITodo[] = [
+		{
+			id: 1,
+			work: '할 일 1',
+		},
+		{
+			id: 2,
+			work: '할 일 2',
+		},
+		{
+			id: 3,
+			work: '할 일 3',
+		},
+		{
+			id: 4,
+			work: '할 일 4',
+		},
+		{
+			id: 5,
+			work: '할 일 5',
+		},
+	];
+
+	return {
+		todoList,
+	};
 };
